de6f8b32bc3247ca639f2539fd98b0990ebf0bd5
KoenMW <89910070+KoenMW@users.noreply.github.com>
2023-04-23 16:57:05 +0200

    Update recentTransactions.vue

--- src/components/recentTransactions.vue
+++ src/components/recentTransactions.vue
@@ -1,8 +1,7 @@
 <template>
     <div class="card bg-dark m-5 p-3">
         <h3 class="text-purple mx-auto">Recente transacties</h3>
-        
-    <transaction/>
+        <transaction/>
     </div>
 </template>
 
