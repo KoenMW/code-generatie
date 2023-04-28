d7d8ef0ae5cdfc252f1e4ce9c7f69e100575d61f
Tlonks <89910091+Tlonks@users.noreply.github.com>
2023-04-28 13:34:17 +0200

    Axios en login verder gedaan en nof een admin pagina

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import allAccounts from '../views/allAccounts.vue'
 import noAccount from '../views/noAccount.vue'
 import configureLimit from '../views/configureLimit.vue'
 import adminTransaction from '../views/adminTransaction.vue'
+import bankOwnAccount from '../views/bankOwnAccount.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -50,6 +51,11 @@ const router = createRouter({
       path: '/adminTransaction',
       name: 'adminTransaction',
       component: adminTransaction
+    },
+    {
+      path: '/bankOwnAccount',
+      name: 'bankOwnAccount',
+      component: bankOwnAccount
     }
 
   ]
---
+++ src/views/bankOwnAccount.vue
@@ -0,0 +1,77 @@
+<style>
+#card {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    margin-left: 20px;
+    margin-right: 20px;
+    border-radius: 10px;
+    padding: 10px;
+
+}
+
+#title {
+    color: #9F82EB;
+}
+
+#mainTitle {
+    color: #9F82EB;
+}
+
+hr {
+    color: #9F82EB;
+}
+
+#functionButton {
+    background-color: #9F82EB;
+}
+
+#functionButton:hover {
+    background-color: #321A72;
+}
+
+#transfer {
+  margin: 0 auto;
+  margin-top: 10%;
+  width: 50%;
+  padding: 10px;
+  border-radius: 5px;
+
+}
+
+#accountName{
+    color: #9F82EB;
+}
+</style>
+
+<template>
+    <a href="/admin" id="backButton" class="btn">
+        Back
+    </a>
+    <div class="container px-4 py-5" id="hanging-icons">
+        <h2 id="title" class="pb-2  ">Novabank account</h2>
+        <hr />
+        <div class="row g-4 py-5 row-cols-1 row-cols-lg-3 ">
+            <div id="transfer" class="bg-dark">
+                <h3 id="accountName">Account</h3>
+                <hr />
+                <p id="accountNumber">Account number: IBAN NL01INHO0000000001</p>
+                <p id="accountBalance">Balance: 102131312</p>
+                
+                    
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+}
+</script>
