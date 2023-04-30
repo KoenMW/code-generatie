95d06adc4f45243c9e7e023fe2f12770cccd7422
KoenMW <89910070+KoenMW@users.noreply.github.com>
2023-04-30 18:32:17 +0200

    Update home.vue

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,21 +1,14 @@
 <template>
-<<<<<<< HEAD
     <div class="container mt-5 mx-auto">
         <div class="row">
             <div class="col mx-5">
                 <userOverview/>
-                <accountOverview/>            
+                <accountOverview/>
+                <totalBalance/>
             </div>
             <div class="col mx-5">
                 <recentTransactions />
             </div>
-=======
-    <div class="d-flex justify-content-around gap-5">
-        <div>
-            <userOverview/>
-            <accountOverview/>
-            <totalBalance/>
->>>>>>> origin/Development
         </div>
     </div>
 </template>
@@ -23,12 +16,8 @@
 <script>
 import userOverview from '../components/userOverview.vue';
 import recentTransactions from '../components/recentTransactions.vue'
-<<<<<<< HEAD
 import accountOverview from '../components/userAccountOverview.vue';
-=======
-import accountOverview from '../components/accountOverview.vue';
 import totalBalance from "@/components/totalBalance.vue";
->>>>>>> origin/Development
 export default {
     components: {
         userOverview,
